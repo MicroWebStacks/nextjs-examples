086e1bac137a48a1c7e8bb48693846a96bf3d4d4
wass <wassim.filali@gmail.com>
2023-02-25 12:17:55 +0100

    updating basic to ssg export test

--- 01_basics/src/pages/static_json.js
+++ 01_basics/src/pages/static_json.js
@@ -2,12 +2,10 @@ import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
 
-console.log("Hello from home page body")
+console.log("Hello from page2 body")
 
-export default function Home() {
-  const name = "01_basics"
-
-  console.log("Hello from home page function")
+export default function Home({name}) {
+  console.log("Hello from page2 function")
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -29,11 +27,18 @@ export default function Home() {
         <div className={styles.thirteen}>
           <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
         </div>
-
       </div>
       <div className={styles.center}>
-        <Link href="/static_json">page2</Link>
+        <Link href="/">home</Link>
       </div>
     </main>
   )
+}
+
+export async function getStaticProps(context) {
+  console.log("Hello from page2 getStaticProps 'on build time'")
+  const name = "01_basics"
+  return {
+    props: {name}, // will be passed to the page component as props
+  }
 }
--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 a sample set of basic patterns for nextjs evaluation and benchmarking
 
 # 01_basics
+
+* Nextjs now forbids full page refresh transitions : https://nextjs.org/docs/messages/no-html-link-for-pages
+* Next executes page 2 get static props even if the page is not called, a sort of prefetch
+* build requires to disable images optimization `images.unoptimized: true` in `next.config.js`
+* export in `out` does not run with vs code live server but runs with
+    ```cmd
+    python3 -m http.server 8000 --directory out
+    ```
+* `getStaticProps` is fetching the data on build time, but then result in needless refetch of the resulting json structure on every client page load. If that is an issue, it is necessary to fall back on client side load and caching. json Data is even loaded two times once on hover prefetch and then on Link click
+
